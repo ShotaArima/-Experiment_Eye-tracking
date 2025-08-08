3c86cab9cea7c5352e3313825150eb4802ff714e
ShotaArima <g2172010@tcu.ac.jp>
2025-08-09 05:58:48 +0900

    create aoi data

---
+++ src/aoi.py
@@ -0,0 +1,42 @@
+def output_aoi_dict():
+    aoi_dict = {
+        1: [
+            {"name": "question_statement", "x": 0, "y": 790, "width": 1920, "height": 380},
+            {"name": "question_graph", "x": 0, "y": 0, "width": 750, "height": 790},
+            {"name": "opt-1", "x": 750, "y": 640, "width": 1170, "height": 150},
+            {"name": "opt-2", "x": 750, "y": 490, "width": 1170, "height": 150},
+            {"name": "opt-3", "x": 750, "y": 340, "width": 1170, "height": 150},
+            {"name": "opt-4", "x": 750, "y": 190, "width": 1170, "height": 150},
+        ],
+
+        2: [
+            {"name": "question_statement", "x": 0, "y": 800, "width": 1920, "height": 380},
+            {"name": "question_graph", "x": 0, "y": 0, "width": 1000, "height": 800},
+            {"name": "opt-1", "x": 1000, "y": 400, "width": 400, "height": 400},
+            {"name": "opt-2", "x": 1400, "y": 400, "width": 520, "height": 400},
+            {"name": "opt-3", "x": 1000, "y": 0, "width": 400, "height": 400},
+            {"name": "opt-4", "x": 1400, "y": 0, "width": 520, "height": 400},
+        ],
+
+        3: [
+            {"name": "question_statement", "x": 0, "y": 750, "width": 1920, "height": 330},
+            {"name": "question_graph", "x": 0, "y": 0, "width": 1000, "height": 750},
+            {"name": "opt-1", "x": 1000, "y": 580, "width": 920, "height": 170},
+            {"name": "opt-2", "x": 1000, "y": 410, "width": 920, "height": 170},
+            {"name": "opt-3", "x": 1000, "y": 240, "width": 920, "height": 170},
+            {"name": "opt-4", "x": 1000, "y": 0, "width": 920, "height": 240},
+        ],
+
+        4: [
+            {"name": "question_statement", "x": 0, "y": 780, "width": 1920, "height": 300},
+            {"name": "question_graph", "x": 0, "y": 0, "width": 1140, "height": 780},
+            {"name": "opt", "x": 1140, "y": 680, "width": 920, "height": 100},
+            {"name": "opt-1", "x": 1140, "y": 580, "width": 920, "height": 100},
+            {"name": "opt-2", "x": 1140, "y": 480, "width": 920, "height": 100},
+            {"name": "opt-3", "x": 1140, "y": 380, "width": 920, "height": 100},
+            {"name": "opt-4", "x": 1140, "y": 280, "width": 920, "height": 100},
+            {"name": "opt-5", "x": 1140, "y": 180, "width": 920, "height": 100},
+            {"name": "opt-6", "x": 1140, "y": 0, "width": 920, "height": 180},
+        ]
+    }
+    return aoi_dict
